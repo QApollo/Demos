d0a2c8af5c5e0057a8f95d4a6f589845e88c9288
ssoegriemsingh <ssoegriemsingh@qualogy.com>
2017-07-28 14:26:21 +0200

    Fixed the Debug class, main method not needed

--- 3117Sprint/3-Sheik/classes/Debug.java
+++ 3117Sprint/3-Sheik/classes/Debug.java
@@ -7,20 +7,13 @@ import java.util.Arrays;
 // Class:
 public class Debug {
 	public static int count = 0;
-	
-	// Main:
-	public static void main(String[] args) {
-		
-	}
-	
-	// Log:
+
 	public static void log(Object s) {
 		count++;
 		System.out.println("Log " + count + ":" + s);
 	}
-	
-	// Log arrays:
-	public static void logArray(String[] a) {
+
+	public static void log(String[] a) {
 		count++;
 		System.out.println("Log " + count + ":" + Arrays.toString(a));
 	}
