63128ccc5c1ee79ebd34b4844787a4202199da71
Kenny Lam <HK_Lam@Live.com>
2017-08-03 13:13:47 +0200

    Demo Chapter 4. Methods and Incapsulations

---
+++ 1732Sprint/1-Kenny/music/Album.java
@@ -0,0 +1,14 @@
+package music;
+
+public class Album {
+
+    private String name;
+
+    public Album(String name) {
+    	this.name = name;
+    }
+
+    public String getAlbumName() {
+        return name;
+    }
+}
