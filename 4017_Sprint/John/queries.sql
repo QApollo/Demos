62782f97b9352b5d86f03beef84c5612d8850cf5
Ivan Bereziuk <ibereziuk@qualogy.com>
2017-09-29 14:24:27 +0200

    delete garbage

--- 4017_Sprint/John/queries.sql
+++ 4017_Sprint/John/queries.sql
@@ -15,19 +15,3 @@ INSERT INTO exams VALUES
 	('Xander', 77, '2017-09-29'),
 	('Sheik', 52, '2017-09-08');
 
-
-SELECT DISTINCT name, score, exam_date,
-	CASE 
-		WHEN (score >= 65) THEN 'Sucess'
-		ELSE 'Fail'
-	END
-		AS Sucess,
-	(score - LAG(score,1, score) OVER (ORDER BY exam_date)) 
-		AS "impovement over pred"
-FROM exams
-ORDER BY exam_date;
-
-
-SELECT INITCAP(name) AS "Certified"
-FROM exams
-WHERE NOT score < 65;
