25f4ab0c53ffca6099424c69cd5c0f68ea076ad1
ssoegriemsingh <ssoegriemsingh@qualogy.com>
2017-11-23 11:49:29 +0100

    initial commit

---
+++ 4817_Sprint/3-Sheik/Demo.java
@@ -0,0 +1,12 @@
+public class Demo {
+
+    public static void main (String[] args) {
+        new Demo();
+    }
+
+    public Demo() {
+        String[] array = new String[10];
+        
+    }
+
+}
