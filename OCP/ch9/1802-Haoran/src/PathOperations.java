e79b61bab7c0a29fab284725abc7175224e5de89
HaoranDing <hding@qualogy.com>
2018-01-05 13:54:07 +0100

    Changed according to feedbacks

--- OCP/ch9/1802-Haoran/src/PathOperations.java
+++ OCP/ch9/1802-Haoran/src/PathOperations.java
@@ -20,6 +20,7 @@ public class PathOperations {
                 }
             });
         } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
@@ -32,6 +33,7 @@ public class PathOperations {
             }
             Files.createDirectories(createPath);
         } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
